708f01c4be691e26b38bf73e49eed21bd6a552cb
Kuba Sajdak <63801032+sajdak00@users.noreply.github.com>
2020-07-07 09:06:45 +0200

    Add files via upload

---
+++ mtpmount.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+#This script needs dmenu and simple-mtpfs to work, so you have to install them before you start using this script
+
+android=$(simple-mtpfs -l)
+choose="$(echo "$android" | dmenu -i -p "Which Android device?")"
+device="$(echo "$choose" | cut -d : -f 1)"
+
+
+androiddev=$(simple-mtpfs -l | cut -d ' ' -f 2)
+mountpoint="$(printf "$androiddev" | dmenu -i -p "Which mountpoint?")"
+mkdir -p ~/Cell/$androiddev && mp=~/Cell/$androiddev
+
+
+mtumt="$(printf "Mount\nUnmount" | dmenu -i -p "Do you want mount or unmount your device?")"
+case "$mtumt" in
+	Mount) simple-mtpfs --device $device $mp
+	;;
+	Unmount) fusermount -u $mp && rmdir ~/Cell/$androiddev
+	;;
+esac
