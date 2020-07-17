3432dd5b2ed980cadf9f980ed12d14535dd95d7f
Kuba Sajdak <63801032+sajdak00@users.noreply.github.com>
2020-07-17 09:06:55 +0200

    Add files via upload

--- mtpmount.sh
+++ mtpmount.sh
@@ -12,9 +12,6 @@ mountpoint="$(printf "$androiddev" | dmenu -i -p "Which mountpoint?")"
 mkdir -p ~/Cell/$androiddev && mp=~/Cell/$androiddev
 
 
-echo "OK" | dmenu -i -p "Tap Allow on your phone if it asks for permission and then press enter"
-
-
 mtumt="$(printf "Mount\nUnmount" | dmenu -i -p "Do you want mount or unmount your device?")"
 case "$mtumt" in
 	Mount) simple-mtpfs --device $device $mp
@@ -22,3 +19,9 @@ case "$mtumt" in
 	Unmount) fusermount -u $mp && rmdir ~/Cell/$androiddev
 	;;
 esac
+
+
+if [ $mtumt = 'Mount' ];
+then
+	echo "OK" | dmenu -i -p "Tap Allow on your phone if it asks for permission and then press enter" && simple-mtpfs --device $device $mp
+fi
